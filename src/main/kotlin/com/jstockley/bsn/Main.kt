10435c24a54ac9e1c9c31317db309133c1240654
Jack Stockley <jack-stockley@uiowa.edu>
2022-08-26 21:49:46 -0500

    Setup check fix

--- src/main/kotlin/com/jstockley/bsn/Main.kt
+++ src/main/kotlin/com/jstockley/bsn/Main.kt
@@ -83,7 +83,7 @@ private fun checkSetup(): Boolean {
         getDataAsStringMap(TWITCH_KEYS)
         getDataAsList(YOUTUBE_KEYS)
         getDataAsIntMap(YOUTUBE_PLAYLISTS)
-        getDataAsStringMap(YOUTUBE_IDS)
+        //getDataAsStringMap(YOUTUBE_IDS)
         getDataAsBooleanMap(YOUTUBE_LIVE_CHANNELS)
         getDataAsList(TWITCH_CHANNELS)
     } catch (e: FileNotFoundException) {
